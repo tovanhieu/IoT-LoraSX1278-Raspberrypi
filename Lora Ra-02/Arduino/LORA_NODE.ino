d089f1c63762e08dd38acefb113fbd2163f8febf
tovanhieu <tovanhieu1996.hl@gmail.com>
2018-12-03 21:19:36 +0700

    Update and rename LORA_SENDERDHT11.ino to LORA_NODE.ino

--- Lora Ra-02/Arduino/LORA_NODE.ino
+++ Lora Ra-02/Arduino/LORA_NODE.ino
@@ -6,6 +6,7 @@
 #include <SPI.h>
 #include <RH_RF95.h>
 #include "dht.h"
+#include <stdio.h>
 
 #define RFM95_CS 10
 #define RFM95_RST 9
@@ -15,28 +16,29 @@
 #define LED 6
 //Define DHT11 Analog Pin
 #define dht_apin A0 
+//Define MQ0 Analog Pin
+#define SMOKEA1 A1
 // Change to 434.0 or other frequency, must match RX's freq!
 #define RF95_FREQ 434.0
 
 // Singleton instance of the radio driver
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
 dht DHT;
+void concatenate_string(char* ,char*);
 void setup() 
 {
+  pinMode(SMOKEA1, INPUT);
   pinMode(RFM95_RST, OUTPUT);
   digitalWrite(RFM95_RST, HIGH);
 
   while (!Serial);
   Serial.begin(9600);
-//  Serial.begin(115200);
   delay(100);
-
   // manual reset
   digitalWrite(RFM95_RST, LOW);
   delay(10);
   digitalWrite(RFM95_RST, HIGH);
   delay(10);
-
   while (!rf95.init()) {
     Serial.println("LoRa radio init failed");
     while (1);
@@ -52,9 +54,9 @@ void setup()
   // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
   // you can set transmitter powers from 5 to 23 dBm:
   // Defaults medium Range after init are 434.0MHz, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
-  //  rf95.setModemConfig(RH_RF95::Bw125Cr45Sf128);
+    rf95.setModemConfig(RH_RF95::Bw125Cr45Sf128);
   //  Fast + short range
-  rf95.setModemConfig(RH_RF95::Bw500Cr45Sf128);
+//  rf95.setModemConfig(RH_RF95::Bw500Cr45Sf128);
 //    Slow + long range Option 1
 //    rf95.setModemConfig(RH_RF95::Bw31_25Cr48Sf512);
 //    Slow + long range Option 2
@@ -70,31 +72,39 @@ int8_t send_ack=0; // flag var
 void loop()
 {
   while (send_ack==0){
-    DHT.read11(dht_apin);
-    digitalWrite(LED, HIGH);
     Serial.println("Send: INF");
-    char radiopacket[4] = "INF";
-    rf95.send((uint8_t *)radiopacket, 4);
-    delay(10);
-    rf95.waitPacketSent();
+    digitalWrite(LED, HIGH);
+    char radiopacket[22] = "INF";
+    float smokevalue = analogRead(SMOKEA1);
+    char radiopacket1[4];
+    dtostrf(smokevalue,3,2,radiopacket1);
+    concatenate_string(radiopacket,radiopacket1);
+    Serial.print("Nong do khong khi: ");
+    Serial.print(smokevalue);
+    DHT.read11(dht_apin);     
     float tem = DHT.temperature;
-    Serial.print("Nhiet do: ");
+    Serial.print(" Nhiet do: ");
     Serial.print(tem);
     Serial.print(" C ");
-    char radiopacket1[4];
+    char radiopacket2[5];
+    dtostrf(tem,3,2,radiopacket2);
+    concatenate_string(radiopacket,radiopacket2);
+//    Serial.println(radiopacket);
     float hum = DHT.humidity;
-    Serial.print("Do am: ");
+    Serial.print(" Do am: ");
     Serial.print(hum);
-    Serial.print(" %");
-    char radiopacket2[4];
-    dtostrf(tem,3,1,radiopacket1);
-    rf95.send((uint8_t *)radiopacket1, sizeof(radiopacket1));
+    Serial.println(" %");
+    char radiopacket3[5];
+    dtostrf(hum,3,2,radiopacket3);
+    concatenate_string(radiopacket,radiopacket3);
+//    Serial.println(radiopacket);
+    rf95.send((uint8_t *)radiopacket, 22);
     delay(10);
     rf95.waitPacketSent();
-    dtostrf(hum,3,1,radiopacket2);
-    rf95.send((uint8_t *)radiopacket2, sizeof(radiopacket2));
-    delay(10);
-    rf95.waitPacketSent();
+    memset(radiopacket, 0, sizeof(radiopacket));
+    memset(radiopacket1, 0, sizeof(radiopacket1));
+    memset(radiopacket2, 0, sizeof(radiopacket2));
+    memset(radiopacket2, 0, sizeof(radiopacket3));
     uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
     uint8_t len = sizeof(buf);
     if (rf95.waitAvailableTimeout(20000)){ 
@@ -123,4 +133,19 @@ void loop()
   }
   digitalWrite(LED, LOW);
   delay(10000);
+}
+void concatenate_string(char *original, char *add)
+{
+   while(*original)
+      original++;
+      *original = 32;
+      original++;
+
+   while(*add)
+   {
+      *original = *add;
+      add++;
+      original++;
+   }
+   *original = '\0';
 }
