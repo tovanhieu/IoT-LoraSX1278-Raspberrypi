25d25c695137120df75a49a0702130ddb1f308e9
To Van Hieu <tovanhieu1996.hl@gmail.com>
2019-03-24 16:25:10 +0700

    Update LORA_NODE_01_encrypted.py

--- Raspberrypi Zero-Node/LORA_NODE_01_encrypted.py
+++ Raspberrypi Zero-Node/LORA_NODE_01_encrypted.py
@@ -29,9 +29,6 @@ from SX127x.LoRaArgumentParser import LoRaArgumentParser
 # Use BOARD 1
 from SX127x.LoRa import LoRa
 from SX127x.board_config import BOARD
-# Use BOARD 2 (you can use BOARD1 and BOARD2 at the same time)
-#from SX127x.LoRa import LoRa2 as LoRa
-#from SX127x.board_config import BOARD2 as BOARD
 import Adafruit_DHT
 import sys
 import math
